d4b8fcca75124f6c8424b620a5d4627e17733e02
mehmetfurkan1460-cmd <mehmetfurkan1460@gmail.com>
2026-03-04 17:41:44 +0300

    added express server

--- index.js.js
+++ index.js.js
@@ -1,3 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("TR54 Bot Aktif");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Web server aktif");
+});
+
 const {
 Client,
 GatewayIntentBits,
